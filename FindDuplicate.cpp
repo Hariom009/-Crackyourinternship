4ca9b2e20f4c611601f065cf78e681f780c7f4c0
Hariom009 <160392683+Hariom009@users.noreply.github.com>
2024-08-08 17:23:56 +0530

    Add files via upload

---
+++ FindDuplicate.cpp
@@ -0,0 +1,15 @@
+ #include<iostream>
+using namespace std;
+#include<vector>
+  int findDuplicate(vector<int>& nums) {
+int n = nums.size();
+int ans;
+        sort(nums.begin(), nums.end());
+        for(int i =0;i<n;i++){
+            if(nums[i] == nums[i+1]){
+                ans = nums[i];
+                break;
+            }
+        }
+        return ans;
+    }
