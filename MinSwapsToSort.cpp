f34721190c397e04e57613bb636ef4f715ef66f7
Hariom009 <160392683+Hariom009@users.noreply.github.com>
2024-10-19 20:55:40 +0530

    Add files via upload

--- MinSwapsToSort.cpp
+++ MinSwapsToSort.cpp
@@ -1,34 +1,42 @@
 #include<iostream>
+#include<vector>
+#include<utility>
 using namespace std;
 
-int minSwap(vector<int> &nums){
-    int n = nums.size();
-    vector<int> temp(n);
-    for(int i =0;i<n;i++){
-        temp[i] = nums[i];
+	int minSwaps(vector<int>&nums)
+	{
+	  int n = nums.size();
+    vector<pair<int, int> > v;
+    for (int i = 0; i < n; i++) {
+        v.push_back(make_pair(nums[i], i));
     }
-    sort(temp.begin(),temp.end());
-    int unequal = 0;
-	   for(int i = 0;i<n;i++){
-	       if(nums[i] != temp[i]){
-	           unequal++;
-	       }
+	  // Time complexity of this sort = O(NlogN)
+	  sort(v.begin(),v.end());
+	  int countswap = 0;
+	  //For this = O(N)
+	   for(int i =0;i<nums.size();i++){
+	       pair<int,int>p = v[i];
+	       int value = p.first;
+	       int index = p.second;
+	       
+// (1,1) (2,2) (3,3) (4,4)
+// next step: (2,2) (1,1) (3,3) (4,4) so i should not increase till its i==index
+
+	     if(i != index){
+	         countswap++;
+	         swap(v[i], v[index]);
+	         i--;
+	     }
 	   }
-	   cout<<unequal<<endl;
-	   if(unequal%2 != 0){
-	       unequal++;
-	   }
-	   unequal = unequal/2;
-	   return unequal;
-}
+	   return countswap;
+	}
 int main(){
-    int n;
-    cout<<"Size of the input vector"<<": ";
-    cin>>n;
-    vector<int>arr(n);
-    for(int i = 0;i<n;i++){
-        cin>>arr[i];
-    }
-    int minswap = minSwap(arr);
+   int n;
+   cin>>n;
+   vector<int>nums(n);
+   for(int i =0;i<n;i++){
+    cin>>nums[i];
+   }
+    int minswap = minSwaps(nums);
     cout<<"Minimum Swap reuired to sort :"<<minswap<<endl;
 }
