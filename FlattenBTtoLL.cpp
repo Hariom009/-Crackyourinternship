11f28a52b04d6f0e94084a50ef1ac7e008592ae3
Hariom009 <160392683+Hariom009@users.noreply.github.com>
2024-10-30 21:33:09 +0530

    Add files via upload

---
+++ FlattenBTtoLL.cpp
@@ -0,0 +1,17 @@
+   #include<iostream>
+   using namespace std;
+   #include"TreeNode.h"
+   class Solution{
+   public:
+   TreeNode* prev = NULL;
+    void flatten(TreeNode* root) {
+        if(root == NULL){
+        return;
+        }
+         flatten(root->right);
+         flatten(root->left);
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
+    }
+   };
